a06ab8daa685b7b71346584d344a69dc0b4c18cf
Sergey Baikov <rayleonard9@gmail.com>
2020-04-22 17:04:41 +0300

    4. candicates.jsp - список кандидатов.[#277286]

--- src/main/java/ru/job4j/Store.java
+++ src/main/java/ru/job4j/Store.java
@@ -9,17 +9,26 @@ public class Store {
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "Vacancy"));
         posts.put(2, new Post(2, "Middle Java Job", "Vacancy"));
         posts.put(3, new Post(3, "Senior Java Job", "Vacancy"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
