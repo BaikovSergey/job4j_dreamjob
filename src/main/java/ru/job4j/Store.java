5e2b63a96a7842348dcc5d0514d1c8bac11506ab
Sergey Baikov <rayleonard9@gmail.com>
2020-04-25 16:10:44 +0300

    1. Реализовать приложения для работы с пользователем.[#277291]

--- src/main/java/ru/job4j/Store.java
+++ src/main/java/ru/job4j/Store.java
@@ -14,6 +14,8 @@ public class Store {
 
     private static AtomicInteger POST_ID = new AtomicInteger(4);
 
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "Vacancy"));
         posts.put(2, new Post(2, "Middle Java Job", "Vacancy"));
@@ -35,8 +37,13 @@ public class Store {
         return candidates.values();
     }
 
-    public void save(Post post) {
+    public void savePost(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+
+    public void saveCandidate(Candidate candidate) {
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
     }
 }
